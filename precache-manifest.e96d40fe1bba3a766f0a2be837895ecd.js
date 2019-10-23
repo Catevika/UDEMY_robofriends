1950f6d18f06483fccacc505893683202aca1542
Catevika <dominique.bello@outlook.fr>
2019-10-23 16:07:13 +0200

    Updates

---
+++ precache-manifest.e96d40fe1bba3a766f0a2be837895ecd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c973b7895b8bedd8862a28b08f2c65e",
+    "url": "/UDEMY_robofriends/index.html"
+  },
+  {
+    "revision": "959ea35696dc803ff5f8",
+    "url": "/UDEMY_robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "877401d02151cf2bdbc8",
+    "url": "/UDEMY_robofriends/static/css/main.8b6bd99c.chunk.css"
+  },
+  {
+    "revision": "959ea35696dc803ff5f8",
+    "url": "/UDEMY_robofriends/static/js/2.4310c7f6.chunk.js"
+  },
+  {
+    "revision": "877401d02151cf2bdbc8",
+    "url": "/UDEMY_robofriends/static/js/main.343bbfb9.chunk.js"
+  },
+  {
+    "revision": "21cbbe0b4a35f956c0aa",
+    "url": "/UDEMY_robofriends/static/js/runtime-main.deaa1c3c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/UDEMY_robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
