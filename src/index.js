74b3c1ee1e283f1c81d71301b6618d4f505ccb1d
Catevika <dominique.bello@outlook.fr>
2019-10-24 16:27:35 +0200

    Second reducer

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { logger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './containers/App';
+import { searchRobots, requestRobots } from './reducers';
 import './index.css';
+import 'tachyons';
 import * as serviceWorker from './serviceWorker';
 
-import 'tachyons';
-
-import { createStore, applyMiddleware } from 'redux';
-import { searchRobots } from './reducers';
+const rootReducer = combineReducers({ searchRobots, requestRobots });
 
 const store = createStore(
-	searchRobots,
-	composeWithDevTools(applyMiddleware(logger))
+	rootReducer,
+	composeWithDevTools(applyMiddleware(thunkMiddleware, logger))
 );
 
 ReactDOM.render(
--- src/reducers.js
+++ src/reducers.js
@@ -1,13 +1,47 @@
-import { CHANGE_SEARCH_FIELD } from './constants.js';
+import {
+	CHANGE_SEARCH_FIELD,
+	REQUEST_ROBOTS_PENDING,
+	REQUEST_ROBOTS_SUCCESS,
+	REQUEST_ROBOTS_FAILED
+} from './constants.js';
 
-const initialState = {
+const initialStateSearch = {
 	searchField: ''
 };
 
-export const searchRobots = (state = initialState, action = {}) => {
+export const searchRobots = (state = initialStateSearch, action = {}) => {
 	switch (action.type) {
 		case CHANGE_SEARCH_FIELD:
 			return { ...state, action, searchField: action.payload };
+		default:
+			return state;
+	}
+};
+
+const initialStateRobots = {
+	isPending: false,
+	robots: [],
+	error: ''
+};
+
+export const requestRobots = (state = initialStateRobots, action = {}) => {
+	switch (action.type) {
+		case REQUEST_ROBOTS_PENDING:
+			return { ...state, action, isPending: true };
+		case REQUEST_ROBOTS_SUCCESS:
+			return {
+				...state,
+				action,
+				robots: action.payload,
+				isPending: false
+			};
+		case REQUEST_ROBOTS_FAILED:
+			return {
+				...state,
+				action,
+				error: action.payload,
+				isPending: false
+			};
 		default:
 			return state;
 	}
