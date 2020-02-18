54ab6c731fe57771ab72df8505c51e8a8679575b
Catevika <dominique.bello@outlook.fr>
2020-02-18 10:56:44 +0100

    Typos amended in actions test

--- src/actions.test.js
+++ src/actions.test.js
@@ -1,20 +1,14 @@
-import {
-	CHANGE_SEARCH_FIELD,
-	REQUEST_ROBOTS_PENDING,
-	REQUEST_ROBOTS_SUCCESS,
-	REQUEST_ROBOTS_FAILED
-} from './constants.js';
+import { CHANGE_SEARCH_FIELD, REQUEST_ROBOTS_PENDING } from './constants.js';
 
 import * as actions from './actions';
 
 import configureStore from 'redux-mock-store';
 import thunkMiddleware from 'redux-thunk';
 
-import nock from 'nock';
 const mockStore = configureStore([thunkMiddleware]);
 
 describe('setSearchField', () => {
-	it('should create an actions to search robots', () => {
+	it('should create an action to search robots', () => {
 		const text = 'Wooo';
 		const expectedcAction = {
 			type: CHANGE_SEARCH_FIELD,
